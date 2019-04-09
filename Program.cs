614812f91bd5c04af6c1d217ff0fe4e5bc7ea92b
NYehor <yehornedbalo@gmail.com>
2019-04-10 00:34:20 +0300

    Create WebApi + React

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MindMap
+namespace Procoder
 {
     public class Program
     {
--- README.md
+++
@@ -1,6 +0,0 @@
-# MindMap
-
-## To deploy MindMap on your pc you need to install:
-- VS Code or Visual Studio 2017
-- .Net Core sdk (version 2.2.104)
-- MySql Community (version 8.0.15)
