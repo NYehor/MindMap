b2c4b26a3c3dc13ef131f05c1bcb31c917b83c2c
LenaSamoylenko <aknelyomas@gmail.com>
2019-05-17 13:31:59 +0300

    add new models

---
+++ Models/Entities/NodeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Procoder.Models
+{
+    public class NodeData
+    {
+        [Column("node_id")]
+        public string Id { get; set; }
+        [Column("pre_node_numb")]
+        public string ParentID { get; set; }
+        [Column("node_name")]
+        public string Name { get; set; }
+        [Column("node_content")]
+        public string Content { get; set; }
+        [Column("large_snippet")]
+        public List<Snippet> LargeSnippet { get; set; }
+        [Column("map_id")]
+        public int mapId { get; set; }
+    }
+}
---
+++ Models/Entities/NodesList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Procoder.Models.Entities
+{
+    public class NodesList
+    {
+        [Column("cur_node_numb")]
+        public string CurrentId { get; set; }
+        [Column("pre_node_numb")]
+        public string ParentID { get; set; }
+        [Column("map_id")]
+        public string MapID { get; set; }
+    }
+}
